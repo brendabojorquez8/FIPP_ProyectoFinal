281a1dc0dc3f1409e24806abedcf2c6494436c83
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-04-06 14:41:31 -0700

    navegación iniciada en balance ingresos

--- app/src/main/java/ibarra/kevin/proyecto/Home.kt
+++ app/src/main/java/ibarra/kevin/proyecto/Home.kt
@@ -1,11 +1,24 @@
 package ibarra.kevin.proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun clickIngresos(){
+        val btnBalanceIngresos= findViewById<TextView>(R.id.txtIngresos)
+        btnBalanceIngresos.setOnClickListener {
+            val pantallaBalanceIngresos = Intent(this, BalanceIngresos::class.java)
+            startActivity(pantallaBalanceIngresos)
+            finish()
+        }
     }
 }
